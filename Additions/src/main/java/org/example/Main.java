12b15c636ef7d9900075348b725f20e6f253459f
Sathwikahub <140336691+Sathwikahub@users.noreply.github.com>
2025-12-04 17:53:00 +0530

    Add files via upload

---
+++ Additions/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5 ;
+        int b = 6;
+        int c;
+        c = a+b;
+        System.out.println("Addition of two numbers" +c);
+    }
+}
