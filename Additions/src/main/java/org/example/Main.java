c8039f35f332a2f3f6146789a7883b9d355173f1
Sathwikahub <140336691+Sathwikahub@users.noreply.github.com>
2025-12-10 10:58:22 +0530

    Update Main.java

--- Additions/src/main/java/org/example/Main.java
+++ Additions/src/main/java/org/example/Main.java
@@ -2,10 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 5 ;
+        int a = 6 ;
         int b = 6;
         int c;
         c = a+b;
-        System.out.println("Addition of two numbers" +c);
+        System.out.println("Addition of two nubers" +c);
     }
 }
